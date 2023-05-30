31c5ff221b826de0948b39003e7cdcab98675484
Maksim T <verdo.ya.ry19@gmail.com>
2023-05-30 20:30:11 +0400

    new name

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int firstSum = 400;
-        int secondBalance = balance + firstSum;
-        if (firstSum > 1000) {
-            int firstBonus = firstSum / 100;
-            System.out.println(firstBonus);
-            System.out.println(secondBalance + firstBonus);
+        int summa = 400;
+        int secondBalance = balance + summa;
+        if (summa > 1000) {
+            int Bonus = summa / 100;
+            System.out.println(Bonus);
+            System.out.println(secondBalance + Bonus);
+
         } else {
-            int secondSum = 1200;
-            int finishBalance = balance + firstSum + secondSum;
-            if (secondSum > 1000) {
-                int secondBonus = secondSum / 100;
-                System.out.println(secondBonus);
-                System.out.println(finishBalance + secondBonus);
-            }
+            System.out.println(secondBalance);
+            System.out.println("Бонусов нет");
         }
     }
 }
